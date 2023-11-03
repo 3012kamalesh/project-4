3c2bc9a8bfa6a57c7f8f130f1d5310f3b1677e1c
3012kamalesh <143986667+3012kamalesh@users.noreply.github.com>
2023-11-02 23:34:42 -0700

    Update pending.py

--- pending.py
+++ pending.py
@@ -1,15 +1,5 @@
 ##project:1 return how many words in sentences
 
-name=input("enter the words")
-name2={}
-for i in name:
-    if i in name2:
-        name2[i]+=1
-    else:
-        name2[i]=1
-for x,y in name2.items():
-    print(f"{x}-{y}")
-
         
   
 
