4025d50f9606443ebbbccb861639354f9a81c8d8
3012kamalesh <143986667+3012kamalesh@users.noreply.github.com>
2023-11-02 23:33:59 -0700

    kamalesh project BMI calucaltor

---
+++ pending.py
@@ -0,0 +1,17 @@
+##project:1 return how many words in sentences
+
+name=input("enter the words")
+name2={}
+for i in name:
+    if i in name2:
+        name2[i]+=1
+    else:
+        name2[i]=1
+for x,y in name2.items():
+    print(f"{x}-{y}")
+
+        
+  
+
+
+
